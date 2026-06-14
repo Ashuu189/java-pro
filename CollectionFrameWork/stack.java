63e7042352ae5075120cd3256ec4b6572296c1e9
Ashutosh <ashut.sarkar8187@gmail.com>
2026-06-14 14:50:32 +0530

    Completed List interface in java Collection Framework

---
+++ CollectionFrameWork/stack.java
@@ -0,0 +1,34 @@
+package CollectionFrameWork;
+import java.util.*;
+public class stack {
+   public static void main(String[] args) {
+    Stack<Integer> s= new Stack<>();
+    //All the methodds of vector will work in stack except the methods of linked list like addFirst(), addLast, removeFirst(), removeLast(), peek(), poll(), offer(), getFirst(), getLast() etc.
+
+    //push() method is used to add an element to the top of the stack.
+      s.push(20);
+      s.push(30);
+      s.push(40);
+      s.push(50);
+      s.push(60);
+    System.out.println(s);
+
+   //pop() method is used to remove an element from the top or last of the stack.
+      s.pop();
+    System.out.println(s);
+
+    //peek() method is used to get the top or last element of the stack without removing it.
+      System.out.println(s.peek());
+
+      //search() method is used to search for an element in the stack and returns its position from the top of the stack. 
+      // If the element is not found, it returns -1.
+      System.out.println(s.search(60));
+      System.out.println(s.search(50));
+
+      //empty() method is used to check if the stack is empty or not,
+      //  it returns true if the stack is empty and false if the stack is not empty.
+      System.out.println(s.empty());
+
+   } 
+
+}
--- ObjectOrientedProgramming/Abstraction/main.java
+++ ObjectOrientedProgramming/Abstraction/main.java
@@ -1,82 +1,82 @@
-package Abstraction;
-abstract class Bird{
-    abstract void eat();
-    abstract void fly();
+// package Abstraction;
+// abstract class Bird{
+//     abstract void eat();
+//     abstract void fly();
 
-    public void drink(){
-        System.out.println("Bird dont drink water without pot");
-    }
+//     public void drink(){
+//         System.out.println("Bird dont drink water without pot");
+//     }
 
-    private void sleeping(){
-        System.out.println("Bird is sleeping");
-    }
+//     private void sleeping(){
+//         System.out.println("Bird is sleeping");
+//     }
 
-    public void getInfo(){
-        sleeping();
-    }
-}
+//     public void getInfo(){
+//         sleeping();
+//     }
+// }
 
-class Sparrow extends Bird{
+// class Sparrow extends Bird{
 
-    @Override
-    void eat() {
-       System.out.println("Sparrow is eating");
-    }
+//     @Override
+//     void eat() {
+//        System.out.println("Sparrow is eating");
+//     }
 
-    @Override
-    void fly() {
-        System.out.println("Sparrow is eating");       
-    }  
-}
+//     @Override
+//     void fly() {
+//         System.out.println("Sparrow is eating");       
+//     }  
+// }
 
-class Crow extends Bird{
+// class Crow extends Bird{
 
-    @Override
-    void eat() {
-        System.out.println("Crow is eating");
-    }
+//     @Override
+//     void eat() {
+//         System.out.println("Crow is eating");
+//     }
 
-    @Override
-    void fly() {
-        System.out.println("Crow is eating");       
-    }
-}
+//     @Override
+//     void fly() {
+//         System.out.println("Crow is eating");       
+//     }
+// }
 
-public class Main{
-    public static void doBirdStuff(Bird b){
-        b.eat();
-        b.fly();
+// public class Main{
+//     public static void doBirdStuff(Bird b){
+//         b.eat();
+//         b.fly();
 
 
-}
-    public static void main(String[] args) {
+// }
+//     public static void main(String[] args) {
        
-    //    doBirdStuff(new Sparrow());
-    //      doBirdStuff(new Crow());
+//     //    doBirdStuff(new Sparrow());
+//     //      doBirdStuff(new Crow());
 
-    //      Bird c= new Sparrow();
-    //      c.drink();
-    //      c.getInfo();
+//     //      Bird c= new Sparrow();
+//     //      c.drink();
+//     //      c.getInfo();
 
-    //2nd Method
-        Bird b=new Sparrow();
-            b.eat();
-            b.fly();
+//     //2nd Method
+//         Bird b=new Sparrow();
+//             b.eat();
+//             b.fly();
 
-        Bird c= new Crow();
-            c.eat();
-            c.fly();
+//         Bird c= new Crow();
+//             c.eat();
+//             c.fly();
 
-        b.getInfo();
-        b.drink();
+//         b.getInfo();
+//         b.drink();
         
 
 
          
 
-    }
+//     }
 
-}
+// }
 
 
 
