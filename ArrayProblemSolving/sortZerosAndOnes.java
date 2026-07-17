02d42415e7a66a50c24dedabf3857cad0d344139
Ashutosh <ashut.sarkar8187@gmail.com>
2026-07-18 00:33:18 +0530

    Done sorting question with two pointers

---
+++ ArrayProblemSolving/sortZerosAndOnes.java
@@ -0,0 +1,34 @@
+package ArrayProblemSolving;
+public class sortZerosAndOnes {
+    public static int[] sortZerosAndOnes(int[] arr){
+        int n=arr.length;
+        int i=0;
+        int j=n-1;
+        
+        while(i<j){
+            if(arr[i]==1 && arr[j]==0){
+                arr[i]=0;
+                arr[j]=1;
+            }
+            if(arr[i]==0){
+                i++;
+            }
+            if(arr[j]==1){
+                j--;
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int [] arr={1,0,1,0,0,1};
+
+        int [] ans=sortZerosAndOnes(arr);
+
+        for (int i: ans){
+            System.out.print(i+" ");
+
+        }
+
+    }
+}
