4b91dc03964c71abf02456cea8768f9104e92c8c
Ashutosh <ashut.sarkar8187@gmail.com>
2026-07-20 23:29:49 +0530

    Done sorting using two pointer

--- ArrayProblemSolving/sortZerosAndOnes.java
+++ ArrayProblemSolving/sortZerosAndOnes.java
@@ -1,34 +1,34 @@
 package ArrayProblemSolving;
+
+// [100010101]->[000001111]
 public class sortZerosAndOnes {
-    public static int[] sortZerosAndOnes(int[] arr){
-        int n=arr.length;
-        int i=0;
-        int j=n-1;
-        
-        while(i<j){
-            if(arr[i]==1 && arr[j]==0){
-                arr[i]=0;
-                arr[j]=1;
-            }
-            if(arr[i]==0){
-                i++;
-            }
-            if(arr[j]==1){
-                j--;
-            }
+   public static int[] sortZeros(int []arr){
+    int i=0;
+    int j=arr.length-1;
+    while(i<j){
+        //cond1: 
+        if(arr[i]==1 && arr[j]==0){
+            //swap
+            arr[i]=0;
+            arr[j]=1;
         }
-        return arr;
-    }
-
-    public static void main(String[] args) {
-        int [] arr={1,0,1,0,0,1};
-
-        int [] ans=sortZerosAndOnes(arr);
-
-        for (int i: ans){
-            System.out.print(i+" ");
-
+        if(arr[i]==0 ){
+            i++;
+        }
+        if( arr[j]==1){ 
+            j--;
         }
 
     }
+    return arr;
+}
+
+public static void main(String[] args) {
+    int arr[]={1,0,0,0,1,0,1,0,1};
+    int []ans=sortZeros(arr);
+
+   for(int value:ans){
+    System.out.print(value);
+   }
+}
 }
